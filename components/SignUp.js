c805eefedb6be02ebb67b3c28e7d3289527656c7
hoangjam <jameson.hoang@gmail.com>
2022-04-08 00:36:54 -0400

    sign up page added, need to add more info still

---
+++ components/SignUp.js
@@ -0,0 +1,106 @@
+import { StatusBar } from "expo-status-bar";
+import { React, Component, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
+
+const SignUp = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.titleText}>ChartMe</Text>
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Name:"
+          placeholderTextColor="#0094FF"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email:"
+          placeholderTextColor="#0094FF"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password:"
+          placeholderTextColor="#0094FF"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.btn_forgot}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => navigation.navigate("Submit")}
+      >
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0094FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn_forgot: {},
+  titleText: {
+    fontSize: 50,
+    color: "white",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 15,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    // alignItems: "center",
+  },
+  TextInput: {
+    height: 40,
+    width: "100%",
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  loginBtn: {
+    width: "60%",
+    backgroundColor: "black",
+    borderRadius: 10,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  loginText: {
+    fontSize: 21,
+    color: "white",
+  },
+});
+
+export default SignUp;
